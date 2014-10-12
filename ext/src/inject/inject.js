8322f7c109993e74891dc62124490f991a8628ca
Revan Sopher <rsopher@gmail.com>
2014-10-12 06:31:29 -0400

    Injecting JS.

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,4 +1,7 @@
 chrome.extension.sendMessage({}, function(response) {
+
+		console.log('fffff');
+
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
@@ -7,6 +10,11 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+
+		// Get side panel image
+		var img = document.querySelector(".infobox.vcard").querySelector("img");
+		img.src = '//127.0.0.1:5000/'+img.src;
+
 
 	}
 	}, 10);
